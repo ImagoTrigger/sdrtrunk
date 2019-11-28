2c8cd8daf240f7fd6799dd09b1f2e13214dd233b
Denny <denny@denny-desktop>
2019-11-28 05:50:59 -0500

    Work in progress on Playlist editor and Radio Reference editor.

--- src/main/java/io/github/dsheirer/gui/JavaFxWindowManager.java
+++ src/main/java/io/github/dsheirer/gui/JavaFxWindowManager.java
@@ -1,18 +1,24 @@
-/*******************************************************************************
- * sdr-trunk
- * Copyright (C) 2014-2018 Dennis Sheirer
+/*
  *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by  the Free Software Foundation, either version 3 of the License, or  (at your option) any
- * later version.
+ *  * ******************************************************************************
+ *  * Copyright (C) 2014-2019 Dennis Sheirer
+ *  *
+ *  * This program is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License, or
+ *  * (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *  * *****************************************************************************
  *
- * This program is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License  along with this program.
- * If not, see <http://www.gnu.org/licenses/>
- *
- ******************************************************************************/
+ */
 
 package io.github.dsheirer.gui;
 
@@ -20,6 +26,8 @@ import com.google.common.eventbus.Subscribe;
 import io.github.dsheirer.eventbus.MyEventBus;
 import io.github.dsheirer.gui.preference.PreferenceEditorViewRequest;
 import io.github.dsheirer.gui.preference.PreferencesEditor;
+import io.github.dsheirer.gui.radioreference.LoginDialog;
+import io.github.dsheirer.gui.radioreference.LoginDialogViewRequest;
 import io.github.dsheirer.preference.UserPreferences;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
@@ -35,8 +43,10 @@ public class JavaFxWindowManager
 {
     private final static Logger mLog = LoggerFactory.getLogger(JavaFxWindowManager.class);
 
+    private JFXPanel mJFXPanel;
     private UserPreferences mUserPreferences;
     private PreferencesEditor mPreferencesEditor;
+    private LoginDialog mLoginDialog;
 
     public JavaFxWindowManager(UserPreferences userPreferences)
     {
@@ -44,6 +54,14 @@ public class JavaFxWindowManager
 
         //Register this class to receive events via each method annotated with @Subscribe
         MyEventBus.getEventBus().register(this);
+    }
+
+    private void createJFXPanel()
+    {
+        if(mJFXPanel == null)
+        {
+            mJFXPanel = new JFXPanel();
+        }
     }
 
     /**
@@ -59,7 +77,7 @@ public class JavaFxWindowManager
     {
         if(mPreferencesEditor == null)
         {
-            new JFXPanel();
+            createJFXPanel();
             Platform.setImplicitExit(false);
             Platform.runLater(() -> {
                 try
@@ -89,6 +107,38 @@ public class JavaFxWindowManager
                 catch(Throwable t)
                 {
                     mLog.error("Error showing existing preferences editor window", t);
+                }
+            });
+        }
+    }
+
+    /**
+     * Processes a request to show the radioreference.com login credentials editor
+     * @param request to view with a callback listener to receive the login credentials
+     */
+    @Subscribe
+    public void process(final LoginDialogViewRequest request)
+    {
+        if(mLoginDialog != null)
+        {
+            mLoginDialog.show();
+        }
+        else
+        {
+            createJFXPanel();
+            Platform.setImplicitExit(false);
+            Platform.runLater(() -> {
+                try
+                {
+                    mLoginDialog = new LoginDialog(mUserPreferences, request.getListener());
+
+                    Stage stage = new Stage();
+                    stage.setOnHidden(event -> mLoginDialog = null);
+                    mLoginDialog.start(stage);
+                }
+                catch(Throwable e)
+                {
+                    mLog.error("Error launching user preferences window", e);
                 }
             });
         }
